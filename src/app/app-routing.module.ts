e04d49a0704448859cf3d2928cf90b8a4452b231
Sallai András <joska@zold.and>
2022-11-16 09:20:43 +0100

    Kezdés

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EmpComponent } from './emp/emp.component';
+import { GitComponent } from './git/git.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'emp', component: EmpComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'git', component: GitComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpComponent } from './emp/emp.component';
+import { AboutComponent } from './about/about.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpComponent,
+    AboutComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
